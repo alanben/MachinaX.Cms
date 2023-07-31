dbc7a30950fe531f4a2d1fb5c8d14758ed448a7c
Alan Benington <alan@xxboom.com>
2023-07-31 19:13:00 +0200

    Update for degenExcel to get first work sheet and handle exception

--- GeneratorX/Degenerate.Excel.asmx.cs
+++ GeneratorX/Degenerate.Excel.asmx.cs
@@ -24,10 +24,10 @@ namespace XXBoom.MachinaX.GeneratorX {
 	using System.Web.Services;
 	using System.Xml;
 
-	/// <summary>
-	/// Excel De-Generator web service
-	/// </summary>
-	[WebService(Name = "DegenerateExcel",
+    /// <summary>
+    /// Excel De-Generator web service
+    /// </summary>
+    [WebService(Name = "DegenerateExcel",
 				Namespace = "http://www.clickclickBOOM.com/MachinaX/GenerateX",
 				Description = "DegenerateExcel Web Service")]
 	public class DegenerateExcel : GenerateBase {
@@ -154,93 +154,107 @@ namespace XXBoom.MachinaX.GeneratorX {
 			xLogger.Debug("degenExcel", "excelfile", excelfile);
 
 			using (ExcelPackage package = new ExcelPackage(excelfile)) {
-				// get the first worksheet in the workbook
-				ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                ExcelWorksheet worksheet = getFirstWorksheet(package);
+                // Read first row - assume these are column names
+                int cols, col = 1;
+                string columns = String.Empty;
+                bool first = true;
+                while (true) {
+                    try {
+                        string val = worksheet.Cells[1, col].Value.ToString();
+                        xLogger.Debug("degenExcel", "col", col, "val", val);
 
-				// Read first row - assume these are column names
-				int cols, col = 1;
-				string columns = String.Empty;
-				bool first = true;
-				while (true) {
-					try { 
-						string val = worksheet.Cells[1, col].Value.ToString();
-						xLogger.Debug("degenExcel", "col", col, "val", val);
+                        columns = String.Concat(columns, first ? "" : "|", val.Replace(' ', '_').ToLower());
+                        if (first) {
+                            first = false;
+                        }
+                    } catch {
+                        xLogger.Debug("degenExcel::ends:", "col", col);
+                        break;
+                    }
+                    col++;
+                    if (col > worksheet.Cells.Columns) {    // safety
+                        break;
+                    }
+                }
+                cols = col;
+                xLogger.Debug("degenExcel", "::cols:", cols, "::columns:", columns);
+                string[] attributes = columns.Split(new char[] { '|' });
 
-						columns = String.Concat(columns, first? "" : "|", val.Replace(' ', '_').ToLower());
-						if (first) {
-							first = false;
-						}
-					} catch {
-						xLogger.Debug("degenExcel::ends:", "col", col);
-						break;
-					}
-					col++;
-					if (col > worksheet.Cells.Columns) {	// safety
-						break;
-					}
-				}
-				cols = col;
-				xLogger.Debug("degenExcel", "::cols:", cols, "::columns:", columns);
-				string[] attributes = columns.Split(new char[] { '|' });
+                // Now read rows based on above attributes (columns), stop on first row with blank first cell
+                XmlElement item;
+                int row = 2;
+                while (true) {
+                    try {
+                        // Row with first column null signifies end of data
+                        if (worksheet.Cells[row, 1].Value == null) {
+                            xLogger.Debug("degenExcel::ends.null:", "row", row);
+                            break;
+                        }
+                        // or... row with first column blank signifies end of data
+                        if (String.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value.ToString())) {
+                            xLogger.Debug("degenExcel::ends.blank:", "row", row);
+                            break;
+                        }
 
-				// Now read rows based on above attributes (columns), stop on first row with blank first cell
-				XmlElement item;
-				int row = 2;
-				while (true) {
-					try {
-						// Row with first column null signifies end of data
-						if (worksheet.Cells[row, 1].Value == null) {
-							xLogger.Debug("degenExcel::ends.null:", "row", row);
-							break;
-						}
-						// or... row with first column blank signifies end of data
-						if (String.IsNullOrWhiteSpace(worksheet.Cells[row, 1].Value.ToString())) {
-							xLogger.Debug("degenExcel::ends.blank:", "row", row);
-							break;
-						}
+                        item = AddNode(_Root, "item", "") as XmlElement;
+                        item.SetAttribute("row", (row - 1).ToString());
+                        item.SetAttribute("status", "notok");
 
-						item = AddNode(_Root, "item", "") as XmlElement;
-						item.SetAttribute("row", (row - 1).ToString());
-						item.SetAttribute("status", "notok");
+                        for (col = 1; col < cols; col++) {
+                            object obj = worksheet.Cells[row, col].Value;
+                            string nme = attributes[(col - 1)];
+                            string val = (obj == null) ? String.Empty : obj.ToString();
+                            //xLogger.Debug("degenExcel", "row", row, "col", col, "nme", nme, "val", val);
 
-						for (col = 1; col < cols; col++) {
-							object obj = worksheet.Cells[row, col].Value;
-							string nme = attributes[(col - 1)];
-							string val = (obj == null)? String.Empty : obj.ToString();
-							//xLogger.Debug("degenExcel", "row", row, "col", col, "nme", nme, "val", val);
+                            item.SetAttribute(nme, val);
+                        }
+                        item.SetAttribute("status", "ok");
+                    } catch {
+                        xLogger.Debug("degenExcel::ends.error:", "row", row);
+                        break;
+                    }
+                    row++;
+                    if (row > worksheet.Cells.Rows) {   // safety
+                        break;
+                    }
+                }
+            }
+        }
 
-							item.SetAttribute(nme, val);
-						}
-						item.SetAttribute("status", "ok");
-					} catch {
-						xLogger.Debug("degenExcel::ends.error:", "row", row);
-						break;
-					}
-					row++;
-					if (row > worksheet.Cells.Rows) {	// safety
-						break;
-					}
-				}
-			}
-		}
+        private ExcelWorksheet getFirstWorksheet(ExcelPackage package) {
+            ExcelWorksheet worksheet;
+            // get the first worksheet in the workbook
+            try {
+                package.Compatibility.IsWorksheets1Based = true;
+                worksheet = package.Workbook.Worksheets[1];
+            } catch (Exception e) {
+                xLogger.Info("getFirstWorksheet", "::error:", e.Message);
+                throw;
+            }
+            if (worksheet == null) {
+                xLogger.Info("getFirstWorksheet", "::error:", "No worksheet found");
+                throw new Exception("No worksheet found");
+            }
 
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="excelfile"></param>
-		/// <param name="dataXml"></param>
-		/// <param name="columns">Parent element of the list of column elements</param>
-		/// <param name="columnElementName">Name of the column element in the list of columns (eg 'subwf_obj')</param>
-		private void degenExcel(FileInfo excelfile, XmlElement columns, string columnElementName) {
+            return worksheet;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="excelfile"></param>
+        /// <param name="dataXml"></param>
+        /// <param name="columns">Parent element of the list of column elements</param>
+        /// <param name="columnElementName">Name of the column element in the list of columns (eg 'subwf_obj')</param>
+        private void degenExcel(FileInfo excelfile, XmlElement columns, string columnElementName) {
 			xLogger.Debug("degenExcel", "excelfile", excelfile);
 
 			using (ExcelPackage package = new ExcelPackage(excelfile)) {
-				
-				// get the first worksheet in the workbook
-				ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                ExcelWorksheet worksheet = getFirstWorksheet(package);
 
-				// Read first row - assume these are column names and match columns
-				int cols, col = 1;
+                // Read first row - assume these are column names and match columns
+                int cols, col = 1;
 				while (true) {
 					try {
 						string val = worksheet.Cells[1, col].Value.ToString().Trim();
